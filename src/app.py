dc6f7cfe43c5c908b2db206534acc3c54ca13d75
Lius <morarolivio@gmail.com>
2024-07-02 19:27:45 +0200

    replace deprecated with lifespan

--- src/app.py
+++ src/app.py
@@ -1,25 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
-from fastapi.logger import logger as fastapi_logger
+from loguru import logger
 
 from src.api.v1.routers import cryptocurrencies
 from src.dependencies.cmc_http_client import start_cmc_http_client, stop_cmc_http_client
 
 
-async def on_start_up() -> None:
-    fastapi_logger.info("on_start_up")
-
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    logger.info("on_start_up")
     start_cmc_http_client()
-
-
-async def on_shutdown() -> None:
-    fastapi_logger.info("on_shutdown")
-
+    yield
+    logger.info("on_shutdown")
     await stop_cmc_http_client()
 
 
-app = FastAPI(
-    on_startup=[on_start_up], on_shutdown=[on_shutdown], title="FastAPI React App"
-)
+app = FastAPI(title="FastAPI React App", lifespan=lifespan)
 app.include_router(cryptocurrencies)
 
 
