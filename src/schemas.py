0ade6b063d51723c4599021cd6b5ff68f5c5412e
Lius <morarolivio@gmail.com>
2024-07-03 22:54:15 +0200

    add example field

--- src/schemas.py
+++ src/schemas.py
@@ -1,16 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class USD(BaseModel):
-    percent_change_24h: float
-    price: float
+    percent_change_24h: float = Field(examples=["-3.70119543"])
+    percent_change_30d: float = Field(examples=["-13.38036914"])
+    price: float = Field(examples=["59648.79175222376"])
+
+
+class Quote(BaseModel):
+    USD: USD
 
 
 class Cryptocurrency(BaseModel):
-    cmc_rank: int
-    id: int
-    quote: dict[str, USD]
-    total_supply: float
-
-    class Config:
-        allow_population_by_field_name = True
+    cmc_rank: int = Field(examples=["1"])
+    id: int = Field(examples=["1"])
+    name: str = Field(examples=["Bitcoin"])
+    quote: Quote
+    total_supply: float = Field(examples=["19719378"])
